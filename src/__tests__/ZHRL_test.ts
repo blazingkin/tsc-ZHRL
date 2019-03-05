da28ee3bd8b9ebb974c353a1d4260e184be18fb6
Alex Gravenor <blazingkin@gmail.com>
2019-03-05 14:55:55 -0800

    Start on builtins

--- src/__tests__/ZHRL_test.ts
+++ src/__tests__/ZHRL_test.ts
@@ -1,5 +1,4 @@
 import * as source from "../ZHRL";
-import { stringify } from "querystring";
 
 test('constant 3 should parse to itself', () => {
     expect(source.ZHRL.parse(3)).toBe(3);
@@ -32,9 +31,30 @@ test('interp IdC should return the correct bound value, or throw error', () => {
     expect(source.ZHRL.interp(new IdC("a"), map)).toEqual(3);
     expect(source.ZHRL.interp(new IdC("b"), map)).toEqual(false);
     expect(() => {source.ZHRL.interp(new IdC("c"), map)}).toThrow("ZHRL: Unbound identifier c");
+});
+
+test('interpreting a lambda expression should yield a CloV', () => {
+    var env = new Map<String, source.ZHRL.Value>();
+    var lam = new source.ZHRL.LamC(["a", "b"], new source.ZHRL.IdC("a"));
+    expect(source.ZHRL.interp(lam, env)).toEqual(new source.ZHRL.CloV(env, lam));
+});
+
+test('function application with not enough variables should fail', () => {
+    var env = new Map<String, source.ZHRL.Value>();
+    var lam = new source.ZHRL.LamC(["a", "b"], 3);
+    var app = new source.ZHRL.AppC(lam, []);
+    expect(() => {source.ZHRL.interp(app, env)}).toThrow("ZHRL: Function called with wrong arity");
 })
 
 test('very simple function application should work', () => {
     var prog = [["lam", ["x"], "x"], 30];
     expect(source.ZHRL.topInterp(prog)).toEqual(30);
+});
+
+test('test add builtin', () => {
+    var add = source.ZHRL.globalEnv.get("+");
+    if (!source.ZHRL.isBuiltin(add)) {
+        throw new Error("Add should be a builtin");
+    }
+    expect(add.operator([2, 3])).toEqual(5);
 });
